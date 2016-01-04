f68fa381f08b14b59b482ab6d1fd4344281b247b
James Seward <james@jamesoff.net>
2016-01-04 19:39:16 +0000

    gg==G

--- f/index.php
+++ f/index.php
@@ -1,46 +1,46 @@
 <?php
-  include_once "../include/_database.phtml";
-  if (!isset($nocount) && databaseConnect()) {
-      $result = @mysql_query("SELECT hits FROM fail_hits");
-      $hits = mysql_result($result,0,"hits");
-      $hits++;
-      $hits_string = "You are the $hits";
-      $bling = (string)$hits;
-      $endChar = $bling{strlen($bling)-1};
-      switch ($endChar) {
-          case "1": $append .= "st"; break;
-          case "2": $append .= "nd"; break;
-          case "3": $append .= "rd"; break;
-          default:
-              $append .= "th";
-      }
+    include_once "../include/_database.phtml";
+    if (!isset($nocount) && databaseConnect()) {
+        $result = @mysql_query("SELECT hits FROM fail_hits");
+        $hits = mysql_result($result,0,"hits");
+        $hits++;
+        $hits_string = "You are the $hits";
+        $bling = (string)$hits;
+        $endChar = $bling{strlen($bling)-1};
+        switch ($endChar) {
+            case "1": $append .= "st"; break;
+            case "2": $append .= "nd"; break;
+            case "3": $append .= "rd"; break;
+            default:
+                $append .= "th";
+        }
 
-      $endChars = substr($bling, strlen($bling)-2);
-      switch ($endChars) {
-          case "11": case "12": case "13": $append = "th"; break;
-      }
+        $endChars = substr($bling, strlen($bling)-2);
+        switch ($endChars) {
+        case "11": case "12": case "13": $append = "th"; break;
+        }
 
-      $hits_string .= "$append stupid person to be directed to this page.";
+        $hits_string .= "$append stupid person to be directed to this page.";
 
-      @mysql_query("UPDATE fail_hits SET hits='$hits'");
+        @mysql_query("UPDATE fail_hits SET hits='$hits'");
 
-  }
-  else {
-      $hits_string = "";
-  }
+    }
+    else {
+        $hits_string = "";
+    }
 
-  $taunts = array(
-    "FAIL",
-    "TAUNT",
-    "CRETIN"
-  );
+    $taunts = array(
+        "FAIL",
+        "TAUNT",
+        "CRETIN"
+    );
 
-  $taunt = $taunts[rand(0,sizeof($taunts)-1)];
+    $taunt = $taunts[rand(0,sizeof($taunts)-1)];
 
-  $fontsize = 150;
-  if (strlen($taunt) > 6) {
-      $fontsize = 120;
-  }
+    $fontsize = 150;
+    if (strlen($taunt) > 6) {
+        $fontsize = 120;
+    }
 ?>
 
 <html>
