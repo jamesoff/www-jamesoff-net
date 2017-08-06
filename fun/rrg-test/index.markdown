369ca2656a4a8fd168a816722023be1500bdf552
James Seward <james@jamesoff.net>
2017-08-06 20:44:57 +0100

    Test for rrg sharing

---
+++ fun/rrg-test/index.markdown
@@ -0,0 +1,39 @@
+---
+layout: page
+status: publish
+published: true
+title: Random Recipe Generator
+author:
+  display_name: jamesoff
+  login: jamesoff
+  email: james@jamesoff.net
+  url: https://jamesoff.net
+author_login: jamesoff
+author_email: james@jamesoff.net
+author_url: https://jamesoff.net
+wordpress_id: 355
+wordpress_url: http://jamesoff.net/site/?page_id=355
+date: '2008-07-04 14:10:52 -0400'
+date_gmt: '2008-07-04 14:10:52 -0400'
+categories:
+  - Uncategorized
+tags: []
+comments: false
+original: /site/fun/random-recipe-generator
+summary: A randomly generated recipe! Manufacture and consume with care.
+---
+Below is a randomly-generated recipe!
+
+<div id="recipe">{% icon fa-cog fa-spin %} [mixing ingredients]</div>
+
+<div id="yum">Yum.</div>
+
+<div id="share"></div>
+
+Insufficiently delicious? <input id="refresh" type="button" value="Refresh!" onclick="fetchRecipe();" /> <kbd>R</kbd>
+
+Insufficiently insane? <input id="refreshinsane" type="button" value="Refresh insane mode!" onclick="fetchInsane();" /> <kbd>I</kbd>
+
+<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<script type="text/javascript" src="/js/rrg.js"></script>
+
---
+++ js/rrg.js
@@ -0,0 +1,124 @@
+var recipe_id;
+
+function fetchInsane() {
+    fetchRecipe(true);
+}
+
+
+function fetchRecipe(insane=false) {
+    var api_url = "https://api.jamesoff.net/recipe";
+    if (insane) {
+        api_url += '-insane'
+    }
+    $.getJSON(api_url, function(data) {
+        $("div#recipe").replaceWith(
+            formatRecipe(data)
+        );
+    } )
+        .fail(function() {
+            formatRecipeError();
+        } );
+}
+
+
+function formatRecipeError() {
+    $("div#recipe").replaceWith("<div id='recipe'>Oh no, dropped all the ingredients on the floor, sorry :(<br /></div>");
+    $("div#yum").replaceWith("<div id='yum' style='text-decoration: line-through'>Yum.</div>");
+}
+
+
+function formatRecipe(data) {
+    var HTML = '<div id="recipe">';
+    HTML += '<div style="float: right">Serves ' + data.serves + '</div>';
+    HTML += '<h2>' + data.title + '</h2>';
+    HTML += 'You will need:<ul class="recipe_needs">';
+    data.ingredients.forEach(function(i) {
+        HTML += "<li>" + i + "</li>";
+    });
+    HTML += "</ul>";
+    HTML += "Instructions:";
+    HTML += "<ol class='recipe_instr'>";
+    data.steps.forEach(function(s) {
+        HTML += "<li>" + s + "</li>";
+    });
+    HTML + "</ol>";
+    HTML += "</div>";
+    return HTML;
+}
+
+
+function fetchSpecificRecipe(recipe_id) {
+    $.getJSON("https://rrg.jamesoff.net/" + recipe_id + ".json", function(data) {
+        $("div#recipe").replaceWith(
+            formatRecipe(data)
+        );
+        $("div#share").replaceWith(
+            "<div id='share'><input id='share' type='button' value='Get share link' onclick='shareRecipe()' /></div>"
+        );
+        recipe_id = data.metadata.recipe_id;
+    } )
+        .fail(function() {
+            formatRecipeError();
+            $("div#share").replaceWith(
+                "<div id='share'></div>"
+            );
+            recipe_id = '';
+        } );
+}
+
+
+function shareRecipe() {
+    if (recipe_id == '') {
+        return false;
+    }
+    $.getJSON("https://api.jamesoff.net/recipe-persist/" + recipe_id, function(data) {
+        giveShareLink();
+    } )
+        .fail(function() {
+            giveShareFailure();
+        } );
+}
+
+
+function giveShareLink() {
+    var share_url = 'https://jamesoff.net/rrg/' + recipe_id;
+    $("div#share").replaceWith(
+        "<div id='share'><a href='" + url + "'>" + share_url + "</a></div>"
+    );
+}
+
+
+function giveShareFailure() {
+    $("div#share").replaceWith(
+        "<div id='share'><input id='share' type='button' value='Get share link' onclick='shareRecipe()' /> An error occurred while generating the share link.</div>"
+    );
+}
+
+
+function loadHandler() {
+    var recipe_id = window.location.hash.substr(1);
+    if (recipe_id == "") {
+        fetchRecipe();
+    }
+    else {
+        fetchSpecificRecipe(recipe_id);
+    }
+}
+
+
+$(window).load(loadHandler());
+
+
+$(document).keydown(function (e) {
+    if (e.which == 82) {
+        // r
+        fetchRecipe();
+        return false;
+    }
+    if (e.which == 73) {
+        // i
+        fetchInsane();
+        return false;
+    }
+});
+
